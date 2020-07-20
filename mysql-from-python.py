e5ab1e43b71ad36715c18be1cf890cc6f5d2323d
junokili <junokili@hotmail.com>
2020-07-20 04:47:43 +0000

    inital file commit

---
+++ mysql-from-python.py
@@ -0,0 +1,18 @@
+import os
+import pymysql
+
+username = os.getenv('junokili')
+
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    with connection.cursor() as cursor:
+        sql = "select * from Artist"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    connection.close()
